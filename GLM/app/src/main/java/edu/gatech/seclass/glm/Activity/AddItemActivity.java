6c5b9567f7f2de19619959d068cdd2a49dcdef08
Thomas Von Heill <tomvonheill@gatech.edu>
2016-10-13 18:53:27 -0500

    Added activities to Manifeest.xml

--- GLM/app/src/main/java/edu/gatech/seclass/glm/Activity/AddItemActivity.java
+++ GLM/app/src/main/java/edu/gatech/seclass/glm/Activity/AddItemActivity.java
@@ -3,6 +3,8 @@ package edu.gatech.seclass.glm.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import edu.gatech.seclass.glm.R;
+
 /**
  * Created by jbuoni on 10/11/16.
  */
@@ -12,5 +14,7 @@ public class AddItemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item);
     }
+
 }
