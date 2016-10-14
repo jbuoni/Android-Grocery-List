c2c68c0a28672d2e359aac706f44932424b7f9fc
Thomas Von Heill <tomvonheill@gatech.edu>
2016-10-13 19:21:14 -0500

    Revert "Added activities to Manifeest.xml"
    This reverts commit 6c5b9567f7f2de19619959d068cdd2a49dcdef08.

--- GLM/app/src/main/java/edu/gatech/seclass/glm/Activity/AddItemActivity.java
+++ GLM/app/src/main/java/edu/gatech/seclass/glm/Activity/AddItemActivity.java
@@ -3,8 +3,6 @@ package edu.gatech.seclass.glm.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import edu.gatech.seclass.glm.R;
-
 /**
  * Created by jbuoni on 10/11/16.
  */
@@ -14,7 +12,5 @@ public class AddItemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_item);
     }
-
 }
